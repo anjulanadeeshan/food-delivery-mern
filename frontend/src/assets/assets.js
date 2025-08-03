e5e33ce2227932b00d814a1e7969a1f94dfc1da7
anjulanadeeshan <mranjula660@gmail.com>
2025-08-04 01:24:09 +0530

    initial commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,12 @@
+import food_logo from '../assets/food_logo.png'
+import search_icon from './search-icon.png'
+import grocery_store from './grocery-store.png'
+
+export const assets = {
+    food_logo,
+    search_icon,
+    grocery_store,
+}
+
+
+
